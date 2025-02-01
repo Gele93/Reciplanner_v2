74ee5b11e6f7ad1b9a14e7cfd3d931fcd7d78853
Gelecsák Tamás <gelecsak.tamas@gmail.com>
2025-02-01 13:26:08 +0100

    deleted leftover comment

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -84,30 +84,3 @@ function Home({ setIsRecipeModal, setIsRecipeModalAdd, setSelectedRecipe, isLogi
 }
 
 export default Home
-
-
-/*
-
-                    <div className='slide-wraper'>
-                        <div className='slide-title'> {`Dishes with ${randomIngredient}`} </div>
-                        <div className='slide-container'>
-                            <div className='slide-elements'>
-                                {recipes.map((recipe) => (
-                                    <div className='slide-recipe' key={recipe.label} onClick={() => openModal(recipe)}>
-                                        <h2 className='slide-recipe-title'>{shortenTitle(recipe.label, 20)}</h2>
-                                        <img className='slide-recipe-img' src={recipe.image}></img>
-                                        <div className='slide-recipe-details'>
-                                            <h3 className='slide-recipe-detail'>{recipe.totalTime} mins</h3>
-                                            <h3 className='slide-recipe-detail'>{Math.round(recipe.calories)} kcal</h3>
-                                        </div>
-                                        <div className='label-container'>
-                                            {recipe.dietLabels.map((label, i) => (
-                                                <h3 key={i} className='recipe-labels'>{label}</h3>
-                                            ))}
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-*/
